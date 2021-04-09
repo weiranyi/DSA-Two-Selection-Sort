bc20a76bece1a0d6878f12d19d1d125cda4b3bff
yiweiran <168578203@qq.com>
2021-04-09 23:05:54 +0800

    实现插入排序算法

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+## 一、选择排序
+
 ### 内容 ：
 - 泛型的比较用到了Comparable接口<E extends Comparable<E>>
 
 ### 收获：
 - Comparable接口：实现具体比较的逻辑
+
+## 二、插入排序
+
+
---
+++ src/main/java/com/github/weiranyi/InsertionSort/InsertionSort.java
@@ -0,0 +1,32 @@
+package com.github.weiranyi.InsertionSort;
+
+/**
+ * @author: https://github.com/weiranyi
+ * @description InsertionSort
+ * @date: 2021/4/9 10:50 下午
+ * @Version 1.0
+ */
+public class InsertionSort {
+    private InsertionSort() {
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // 将arr[i]插入到合适的位置
+            // j-1要存在，j-1>0;将指针向前移，j--
+            for (int j = i; j - 1 >= 0; j--) {
+                if (arr[j].compareTo(arr[j - 1]) < 0) {
+                    swap(arr,j,j-1);
+                }else {
+                    break;
+                }
+            }
+        }
+    }
+    private static <E> void swap(E[] arr, int i, int j) {
+        E swap = arr[i];
+        arr[i] = arr[j];
+        arr[j] = swap;
+    }
+
+}
